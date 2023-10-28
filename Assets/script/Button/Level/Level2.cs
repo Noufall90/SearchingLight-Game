2648569af17a8166a9ae4ca1ac86e4483fe55ebe
Noval ian <andrianau26@gmail.com>
2023-10-29 04:55:30 +0800

    menu level

---
+++ Assets/script/Button/Level/Level2.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level2 : MonoBehaviour
+{
+    public void LoadScene()
+    {
+        SceneManager.LoadScene("Level2");
+    }
+}
