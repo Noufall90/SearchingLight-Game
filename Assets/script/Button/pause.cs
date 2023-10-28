0f3f4c81056cfaeff6f2cb5baffa54c571e12b41
Noval ian <andrianau26@gmail.com>
2023-10-29 02:35:26 +0800

    pause

---
+++ Assets/script/Button/pause.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] GameObject pauseMenu;
+
+    public void PauseMenu()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void Lanjut()
+    {
+        pauseMenu.SetActive(false);
+        Time.timeScale = 1f;
+    }
+
+    public void Keluar()
+{
+    Time.timeScale = 1f;
+    SceneManager.LoadScene("game"); // Menggunakan nama scene "room" untuk memuatnya
+}
+
+}
