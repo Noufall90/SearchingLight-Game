6529ab298b69b8a0b61578617c2930568d54d129
princenizroh <princenizroh7@gmail.com>
2023-10-29 03:27:32 +0800

    Scene Manager

---
+++ Assets/script/NextLevel/MoveToNextLevel.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MoveToNextLevel : MonoBehaviour
+{
+    public int nextSceneLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextSceneLoad = SceneManager.GetActiveScene().buildIndex + 1;
+    }
+    
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+      if (Score.ScoreNum == 2){
+      
+        if (other.gameObject.tag == "Player")
+        {
+           SceneController.instance.NextLevel(); 
+        }
+      } 
+    }
+    
+}
