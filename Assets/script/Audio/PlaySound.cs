0fe86d76ddb1242a5caaa9a5317173b239a8f609
Noval ian <andrianau26@gmail.com>
2024-09-14 20:36:43 +0800

    enemy

---
+++ Assets/script/Audio/PlaySound.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaySound : MonoBehaviour
+{
+    private AudioSource source;
+    private Collider2D soundTrigger;
+
+    void Awake()
+    {
+        source = GetComponent<AudioSource>();
+        source.loop = true;
+        soundTrigger = GetComponent<Collider2D>();
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player")) 
+        {
+            source.Play(); 
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            source.Stop();
+        }
+    }
+}
